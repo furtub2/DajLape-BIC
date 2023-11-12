7c57222b42687405d12719a38f55967f71ab6254
Jarosław Bigaj <jaroslaw.bigaj@rd.technosystem.pl>
2023-11-12 12:10:39 +0100

    animal card initial change home page

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ cd src/frontend
 npm i
 npm run dev
 ```
+
+### In projcet for now:
+- You can switch between dummy pet card or skeleton card: In Home Page change 
+```ts
+const isLoading = ref(false);
+```
+to
+```ts
+const isLoading = ref(true);
+```
---
+++ src/frontend/src/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+    <div v-if="!isLoading">
+      <AnimalCardComponent />
+    </div>
+    <div v-else>
+      <SkeletonAnimalCardComponent />
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import AnimalCardComponent from 'components/animal-card/AnimalCardComponent.vue';
+import SkeletonAnimalCardComponent from 'src/components/animal-card/SkeletonAnimalCardComponent.vue';
+import { ref } from 'vue';
+
+const isLoading = ref(false);
+</script>
