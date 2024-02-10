5757a9ec6d2e85b3e9825894755a952ba03bda40
Vladyslav Ishchuk <vladvlad5417@gmail.com>
2024-02-10 23:07:07 +0100

    rebuild index.ts file

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,15 +1,7 @@
-import { startServer } from './server'
-import { config } from './config'
-import { prisma } from './database'
-async function main() {
-await startServer(config.server)
-}
-main()
-.then(async () => {
-await prisma.$disconnect()
-})
-.catch(async (e) => {
-console.error(e)
-await prisma.$disconnect()
-process.exit(1)
-})
+import { startServer } from './server';
+
+const port = 3000;
+const corsOptions = { origin: '*' }; 
+const limiter = { time: 15 * 60 * 1000, max: 100 }; 
+
+startServer({ port, corsOptions, limiter });
