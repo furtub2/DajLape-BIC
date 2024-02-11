5f0def6b2222535ce95149b08c6c696f15e946cb
Vladyslav Ishchuk <vladvlad5417@gmail.com>
2024-02-12 00:32:00 +0100

    add offer route for admin

--- src/backend/src/routes/adminRoutes.ts
+++ src/backend/src/routes/adminRoutes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { createUser } from '../controlllers/adminController';
+import { createOffer } from '../controlllers/adminController';
 //import { verifyToken, isAdmin } from '../middlewares/authMiddleware'; 
 
 const router = express.Router();
 
 router.post('/users', /*verifyToken, isAdmin,*/ createUser);
+
+router.post('/offers', /*authenticate, authorize('admin'),*/ createOffer);
+
 
 export default router;
