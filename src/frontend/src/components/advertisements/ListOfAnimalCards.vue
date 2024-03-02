08719f06002b5f3898df90ba6db994cb00beb14b
Jarosław Bigaj <jaroslaw.bigaj@rd.technosystem.pl>
2024-03-02 14:19:22 +0100

    feat: add promoted list to home page

--- src/frontend/src/components/advertisements/ListOfAnimalCards.vue
+++ src/frontend/src/components/advertisements/ListOfAnimalCards.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
-      <div v-if="!isLoading">
-      <AnimalCardComponent />
+  <div v-for="card in cards" :key="card.id" class="q-ma-sm">
+    <div v-if="!isLoading" >
+      <AnimalCardComponent :name="card.name"/>
     </div>
     <div v-else>
       <SkeletonAnimalCardComponent />
     </div>
-    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import AnimalCardComponent from 'components/animal-card/AnimalCardComponent.vue';
@@ -14,4 +14,5 @@ import SkeletonAnimalCardComponent from 'src/components/animal-card/SkeletonAnim
 import { ref } from 'vue';
 
 const isLoading = ref(false);
+const cards = [{id:1,name:'Fafik'},{id:2,name:'Bolek'},{id:3,name:'Krotka'},{id:4,name:'Azazel'},{id:5,name:'Sysktus'},{id:6,name:'Leonida'}]
 </script>
