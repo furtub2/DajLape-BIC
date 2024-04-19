ace4adec983730429795808983e6d29a7dece295
Vladyslav Ishchuk <vladvlad5417@gmail.com>
2024-04-19 21:06:38 +0200

    token validation added

--- src/backend/src/middlewares/authMiddleware.ts
+++ src/backend/src/middlewares/authMiddleware.ts
@@ -1,5 +1,20 @@
-// export const verifyToken = (req, res, next) => {
-//   };
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
 
-// export const isAdmin = (req, res, next) => {
-//   };
+const SECRET_KEY = process.env.JWT_SECRET || 'your_secret_key';
+
+export const validateToken = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided, authorization denied' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Token is not valid' });
+    }
+};
--- src/backend/src/routes/index.ts
+++ src/backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import adminRoutes from './adminRoutes'
 import authRoutes from './authRoutes'
 import shelterRoutes from './shelterRoutes'
 import userRoutes from './userRoutes'
+import { validateToken } from '../middlewares/authMiddleware'
 
 const router = express.Router()
 // middleware
@@ -19,8 +20,8 @@ router.use('/admin', adminRoutes);
 
 router.use('/auth', authRoutes);
 
-router.use('/shelter', shelterRoutes)
+router.use('/shelter', validateToken, shelterRoutes)
 
-router.use('/user', userRoutes)
+router.use('/user', validateToken, userRoutes)
 
 export default router
