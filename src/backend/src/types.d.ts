66ee1054a40c92dd80f9cafa137b3d84054abf66
Vladyslav Ishchuk <vladvlad5417@gmail.com>
2024-04-20 15:00:42 +0200

    userPaylog typing added

--- src/backend/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-// Assuming UserPayload is defined elsewhere in your project, you might need to import it
-import { User } from '@prisma/client';
-import { Request } from 'express';
-
-declare module 'express-serve-static-core' {
-  export interface Request {
-    user?: User; // Optionally make it nullable with '?'
-  }
-}
