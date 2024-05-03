96f01f3cbb8929595977c392133b305e7d2cc293
Jarosław Bigaj <jaroslaw.bigaj@rd.technosystem.pl>
2024-05-03 12:46:14 +0200

    feat: change toolbar

--- src/frontend/src/layouts/MainLayout.vue
+++ src/frontend/src/layouts/MainLayout.vue
@@ -1,37 +1,17 @@
 <template>
   <q-layout view="hLh lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
+      <q-toolbar glossy>
         <q-toolbar-title
           style="cursor: pointer"
-          @click="() => console.log('title')"
-        >
-          Daj Lape
+          @click="redirectToHomePage">
+          <q-icon name="img:src/assets/Logo.svg" size="xl"/>  Podaj Łapę
         </q-toolbar-title>
+        <q-space/>
+        <q-btn label="Sign In" flat @click="router.push('/LoginPage')"/>
+        <q-btn label="Sign Up" flat @click="console.log('register')"/>
       </q-toolbar>
     </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <SearchBar />
       <router-view />
@@ -40,60 +20,16 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import EssentialLink, {
-  EssentialLinkProps,
-} from 'components/EssentialLink.vue';
 import SearchBar from 'src/components/SearchBar.vue';
+import { useRouter } from 'vue-router';
 
-const essentialLinks: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-];
+const router = useRouter()
+// const leftDrawerOpen = ref(false);
 
-const leftDrawerOpen = ref(false);
+// function toggleLeftDrawer() {
+//   leftDrawerOpen.value = !leftDrawerOpen.value;
+// }
 
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
+
+const redirectToHomePage = () => router.push('/')
 </script>
