73edc4188e7d2b54ef5597fe1dd08633d39fa26d
Jarosław Bigaj <jaroslaw.bigaj@rd.technosystem.pl>
2024-05-03 10:57:11 +0200

    feat: add back function

--- src/frontend/src/pages/MoreInfoPage.vue
+++ src/frontend/src/pages/MoreInfoPage.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page>
     <q-card >
+      <q-btn color="primary" flat icon="arrow_back" @click="returnToPreviousSite()"/>
       <ImageSection />
     </q-card>
     <q-card> Contact </q-card>
@@ -8,4 +9,10 @@
 </template>
 <script setup lang="ts">
 import ImageSection from 'components/ImageSection/ImageSection.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const returnToPreviousSite = () => router.back()
+
 </script>
