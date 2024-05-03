9fefe79739ab511cd4e96157fc0f333f8b098d4e
Jarosław Bigaj <jaroslaw.bigaj@rd.technosystem.pl>
2024-05-03 11:30:11 +0200

    feat: add name for more info

--- src/frontend/src/pages/MoreInfoPage.vue
+++ src/frontend/src/pages/MoreInfoPage.vue
@@ -1,10 +1,28 @@
 <template>
-  <q-page>
-    <q-card >
-      <q-btn color="primary" flat icon="arrow_back" @click="returnToPreviousSite()"/>
-      <ImageSection />
+  <q-page padding >
+    <q-btn color="primary" flat icon="arrow_back" @click="returnToPreviousSite()"/>
+    <div class="row q-gutter-md">
+    <div class="col-7 q-gutter-sm">
+      <q-card >
+        <div class="text-h4 q-pa-sm">Name</div>
+      <ImageSection class="q-pa-sm"/>
     </q-card>
-    <q-card> Contact </q-card>
+    <q-card>
+        Description
+      </q-card>
+    </div>
+    <div class="col-4 q-gutter-sm">
+      <q-card> Contact </q-card>
+
+      <q-card >Shelter Info</q-card>
+      <q-card>Localization</q-card>
+    </div>
+    <div>
+      <q-card>Other proposition</q-card>
+    </div>
+  </div>
+
+
   </q-page>
 </template>
 <script setup lang="ts">
